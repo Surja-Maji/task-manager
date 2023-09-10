5eb3946ec1954ee643cb7e6fc6ec874479bf5ae7
Surja Maji <surjamaji9011@gmail.com>
2023-09-10 11:45:57 +0530

    first commit

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -13,7 +13,7 @@ function TodoInput(props) {
       <input
         type="text"
         className="input-box-todo"
-        placeholder="Enter your task"
+        placeholder="Enter your tasks"
         value={inputText}
         onChange={e=>{
             setInputText(e.target.value)
